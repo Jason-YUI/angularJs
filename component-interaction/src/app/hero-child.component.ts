5db122ec4b45163466c988ea5d0ab95c2dd8d6ad
jianghao <jiang.hao@ikcrm.com>
2017-08-24 15:11:56 +0800

    add

--- component-interaction/src/app/hero-child.component.ts
+++ component-interaction/src/app/hero-child.component.ts
@@ -1,15 +1,14 @@
 import { Component, Input } from '@angular/core';
-
 import { Hero } from './hero';
 
 @Component({
-  selector: 'hero-child',
-  template: `
-    <h3>{{hero.name}} says:</h3>
-    <p>I, {{hero.name}}, am at your service, {{masterName}}.</p>
+  selector: 'hello-child',
+  template: `      
+      <h3>{{hero.name}} says:</h3>
+      <p>I, {{hero.name}}, am at your service, {{masterName}}.</p>
   `
 })
 export class HeroChildComponent {
   @Input() hero: Hero;
-  @Input('master') masterName: string;
+  @Input('master') masterName: string
 }
