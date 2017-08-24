31b31135335e2c40dc532c011a2ae4acb3fdb217
jianghao <jiang.hao@ikcrm.com>
2017-08-24 11:46:36 +0800

    add

--- angular-module-jxc/src/app/module.ts
+++ angular-module-jxc/src/app/module.ts
@@ -1,13 +1,17 @@
 //根模块
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { Directive } from '@angular/core';
 @NgModule({
     imports: [BrowserModule],
     providers: [Logger],
     declarations: [ AppComponent ],
     exports:      [ AppComponent ],
     bootstrap:    [ AppComponent ]
+})
+
+@Directive({
+    outputs: ['click: myClick']
 })
 
 export class AppModule{ }
